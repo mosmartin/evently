4656c3eb62c6a3ac8f40656bf17861c2237a7869
Martin Macharia <mosmartin5@gmail.com>
2024-01-22 11:14:51 +0100

    Create the Get and Post test APIs

--- main.go
+++ main.go
@@ -4,18 +4,37 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mosmartin/evently/models"
 )
 
 func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
-	
+	server.POST("/events", createEvent)
+
 	server.Run(":8080")
 }
 
 func getEvents(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello World!",
-	})
+	events := models.GetEvents()
+
+	c.JSON(http.StatusOK, events)
+}
+
+func createEvent(c *gin.Context) {
+	var event models.Event
+	err := c.ShouldBindJSON(&event)
+	if err != nil {
+		// c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request body"})
+		return
+	}
+
+	event.ID = len(models.GetEvents()) + 1
+	event.UserID = 1
+
+	event.Save()
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Event successfully created", "event": event})
 }
